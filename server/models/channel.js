2cf865437ecfd5344ca1d2e39e3c9614666526c7
Rikka <rikkambp@Rikkas-MacBook-Pro.local>
2018-07-22 18:57:38 -0400

    channel schema updated with all crud

--- server/models/channel.js
+++ server/models/channel.js
@@ -2,20 +2,15 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 let ChannelSchema = new Schema({
-  owner: [{
-    _id: {type: String, ref: 'User'},
-    isBlocked: Boolean
-  }],
+  owner: { type: String, ref: 'User' },
   isActive: Boolean,
   createDate: Date,
   name: String,
   members: [{
-    _id: {type: String, ref: 'User'},
-    isBlocked: Boolean
+    _id: { type: String, ref: 'User' }
   }],
   messages: [{
-    _id: {type: String, ref: 'Message'},
-    isBlocked: Boolean
+    _id: { type: String, ref: 'Message' }
   }]
 
 })
@@ -23,6 +18,51 @@ let ChannelSchema = new Schema({
 ChannelSchema.statics.addChannel = (channel) => {
   return new Promise((resolve, reject) => {
     channel.save((err, item) => {
+      if (err) reject(err)
+      else resolve(item)
+    })
+  })
+}
+
+ChannelSchema.statics.updateChannel = (_id, channel) => {
+  return new Promise((resolve, reject) => {
+    this.findByIdAndUpdate(_id, channel, (err, item) => {
+      if (err) reject(err)
+      else resolve(item)
+    })
+  })
+}
+
+ChannelSchema.statics.disableChannel = (_id, channel) => {
+  return new Promise((resolve, reject) => {
+    this.findByIdAndRemove(_id, channel, (err, item) => {
+      if (err) reject(err)
+      else resolve(item)
+    })
+  })
+}
+
+ChannelSchema.statics.getAllChannels = (channel) => {
+  return new Promise((resolve, reject) => {
+    this.find(channel, (err, item) => {
+      if (err) reject(err)
+      else resolve(item)
+    })
+  })
+}
+
+ChannelSchema.statics.getChannelById = (_id, channel) => {
+  return new Promise((resolve, reject) => {
+    this.findOne(_id, (err, item) => {
+      if (err) reject(err)
+      else resolve(item)
+    })
+  })
+}
+
+ChannelSchema.statics.getChannelByOwner = (owner, channel) => {
+  return new Promise((resolve, reject) => {
+    this.findOne(owner, (err, item) => {
       if (err) reject(err)
       else resolve(item)
     })
