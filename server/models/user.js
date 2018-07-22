e9d5922258898bd8dbeffbc7931a87dec9183448
Wyatt <paquinwyatt@gmail.com>
2018-07-22 19:47:36 -0400

    Added getUser method

--- server/models/user.js
+++ server/models/user.js
@@ -29,12 +29,32 @@ UserSchema.statics.addUser = (User) => {
 
 UserSchema.statics.updateUser = (id, User) => {
   return new Promise((resolve, reject) => {
-    User.findByIdAndUpdate(id, {$set: { firstName: User.firstName, lastName: User.lastName }}, { new: true }, function (err, user) {
+    User.findByIdAndUpdate(id, { $set: { firstName: User.firstName, lastName: User.lastName } }, { new: true }, function (err, user) {
       if (err) reject(err)
       else resolve(user)
     })
   })
 }
+
+UserSchema.statics.getAllUsers = (User) => {
+  return new Promise((resolve, reject) => {
+    User.find((err, users) => {
+      if (err) reject(err)
+      else resolve(users)
+    })
+  })
+}
+
+UserSchema.statics.getUser = (id, User) => {
+  return new Promise((resolve, reject) => {
+    User.findById(id, (err, user) => {
+      if (err) reject(err)
+      else resolve(user)
+    })
+  })
+}
+
+
 
 const User = mongoose.model('User', UserSchema)
 export default User
