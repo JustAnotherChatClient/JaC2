3f72f58b45fa38ef29e66b01de6377ff28d70811
Kyle Fontaine <kjfonatine@email.neit.edu>
2018-07-22 20:40:47 -0400

    Working on the Menu. Need to find a way to change the values.

--- client/src/main/index.js
+++ client/src/main/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import { app, Menu, BrowserWindow } from 'electron'
+import { app, Menu, BrowserWindow, MenuItem } from 'electron'
 // BrowserWindow
 
 /**
@@ -49,19 +49,25 @@ app.on('activate', () => {
 
 const template = [
   {
-    label: 'Edit',
+    label: 'File',
     submenu: [
-      { role: 'undo' },
-      { role: 'redo' },
-      { type: 'separator' },
-      { role: 'cut' },
-      { role: 'copy' },
-      { role: 'paste' },
-      { role: 'pasteandmatchstyle' },
-      { role: 'delete' },
-      { role: 'selectall' }
+      { role: 'close' }
     ]
   },
+  // {
+  //   label: 'Edit',
+  //   submenu: [
+  //     { role: 'undo' },
+  //     { role: 'redo' },
+  //     { type: 'separator' },
+  //     { role: 'cut' },
+  //     { role: 'copy' },
+  //     { role: 'paste' },
+  //     { role: 'pasteandmatchstyle' },
+  //     { role: 'delete' },
+  //     { role: 'selectall' }
+  //   ]
+  // },
   {
     label: 'View',
     submenu: [
@@ -88,39 +94,39 @@ const template = [
     submenu: [
       {
         label: 'Learn More',
-        click () { require('electron').shell.openExternal('https://electronjs.org') }
+        click() { require('electron').shell.openExternal('https://electronjs.org') }
       }
     ]
   }
 ]
 
-if (process.platform === 'darwin') {
-  template.unshift({
-    label: app.getName(),
-    submenu: [
-      { role: 'about' },
-      { type: 'separator' },
-      { role: 'services', submenu: [] },
-      { type: 'separator' },
-      { role: 'hide' },
-      { role: 'hideothers' },
-      { role: 'unhide' },
-      { type: 'separator' },
-      { role: 'quit' }
-    ]
-  })
+// if (process.platform === 'darwin') {
+//   template.unshift({
+//     label: app.getName(),
+//     submenu: [
+//       { role: 'about' },
+//       { type: 'separator' },
+//       { role: 'services', submenu: [] },
+//       { type: 'separator' },
+//       { role: 'hide' },
+//       { role: 'hideothers' },
+//       { role: 'unhide' },
+//       { type: 'separator' },
+//       { role: 'quit' }
+//     ]
+//   })
 
-  // Edit menu
-  template[1].submenu.push(
-    { type: 'separator' },
-    {
-      label: 'Speech',
-      submenu: [
-        { role: 'startspeaking' },
-        { role: 'stopspeaking' }
-      ]
-    }
-  )
+//   // Edit menu
+//   template[1].submenu.push(
+//     { type: 'separator' },
+//     {
+//       label: 'Speech',
+//       submenu: [
+//         { role: 'startspeaking' },
+//         { role: 'stopspeaking' }
+//       ]
+//     }
+//   )
 
   // Window menu
   template[3].submenu = [
