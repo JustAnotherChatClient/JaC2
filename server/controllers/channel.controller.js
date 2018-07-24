31234debde0b6361525239be54a1fff51de8dc7f
Rikka <rikkambp@Rikkas-MacBook-Pro.local>
2018-07-23 20:04:26 -0400

    added GET by ID, PUT, PUT by ID

--- server/controllers/channel.controller.js
+++ server/controllers/channel.controller.js
@@ -1,13 +1,15 @@
 import Channel from '../models/channel'
 
 const newChannel = (req, res) => {
-  let channel = new Channel(req.body)
-  Channel.addChannel(channel)
-    .then(newChannel => {
-      res.status(200).json({status: 200, data: newChannel, message: 'Ok'})
-    }).catch(err => {
-      res.status(500).json({status: 500, message: err.message})
-    })
+  if (req.body) {
+    let channel = new Channel(req.body)
+    Channel.addChannel(channel)
+      .then(newChannel => {
+        res.status(200).json({ status: 200, data: newChannel, message: 'Ok' })
+      }).catch(err => {
+        res.status(500).json({ status: 500, message: err.message })
+      })
+  } else throw new Error('unsuccessful. please check for valid input')
 }
 
 export default {
--- server/routes/api/channel.router.js
+++ server/routes/api/channel.router.js
@@ -3,7 +3,16 @@ import ChannelController from '../../controllers/channel.controller'
 const routes = (router) => {
   router.post('/channel', ChannelController.newChannel)
   router.get('/channel', (req, res) => {
-    res.status(200).json({ connected: 'OK!', router: 'test' })
+    res.status(200).json({ connected: 'OK!', router: 'test-get' })
+  })
+  router.get('/channel/:id', (req, res) => {
+    res.status(200).json({ connected: 'OK!', router: 'test-get-id' })
+  })
+  router.put('/channel', (req, res) => {
+    res.status(200).json({ connected: 'OK!', router: 'test-put' })
+  })
+  router.put('/channel/:id', (req, res) => {
+    res.status(200).json({ connected: 'OK!', router: 'test-put-id' })
   })
   return router
 }
