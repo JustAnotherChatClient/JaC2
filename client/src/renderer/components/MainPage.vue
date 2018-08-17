ecf99c7f44af960e64e95a465e9ceabf37cfde8f
Rikka <rikkambp@Rikkas-MacBook-Pro.local>
2018-08-16 20:10:04 -0400

    submit button and sample user message

--- client/src/renderer/components/MainPage.vue
+++ client/src/renderer/components/MainPage.vue
@@ -14,19 +14,49 @@
     </div>
   </div>
   <div class="column is-flex is-paddingless">
-    <div class='chat-area' style="height: 83%">
+    <div class='chat-area' style="height: 80%; padding-top: 50px">
+      <div class="columns">
+        <div class="column is-narrow ">
+          <div class='chat_img' style="height: 50px; width: 50px"> <img src="https://cdn1.iconfinder.com/data/icons/social-messaging-productivity-1-1/128/gender-male2-512.png" alt="test">
+          </div>
+        </div>
+        <div class="column is-flex ">
+          <h3 class="subtitle is-5"><b>Username</b></h3> 
+          <p> Test Message </p>
+        </div>
+        <div class="column is-flex is-paddingless has-text-centered">
+          
+          </div>
+      </div>
+      <!-- <div class='chat_img' style="height: 50px; width: 50px"> <img src="https://cdn1.iconfinder.com/data/icons/social-messaging-productivity-1-1/128/gender-male2-512.png" alt="test"> <p> Test Message </p></div> -->
     </div>
     <article class="media">
       <div class="media-content">
         <div class='container'>
           <div class="field">
+            <div class="columns">
+              <div class="column is-flex">
             <p class="control">
               <textarea class="textarea is-hovered is-flex" placeholder="Message" rows='3'></textarea>
             </p>
-          </div>
+            </div>
+            <div class='column is-narrow'>
+              <a class="button is-large">
+    <span class="icon is-large">
+      <i class="fas fa-angle-right fa-5x"></i>
+    </span>
+  </a>
+            </div>
+            </div>
         </div>
       </div>
+      </div>
     </article>
+  </div>
+  <div class="column is-narrow ">
+    <div class="box" style="height: 100%; width: 60px;">
+      <p class="title">Spacer</p>
+    </div>
   </div>
 </div>
 </div>
@@ -54,6 +84,7 @@ export default {
 
 .chat-area {
   flex: 1 0 auto;
+  overflow-x: scroll;
 }
 
 .media {
@@ -64,5 +95,11 @@ export default {
   margin: 0.0rem;
   padding: 0%;
 }
+
+.textarea{
+  padding: 4px;
+  padding-bottom: 40px;
+}
+
 </style>
 
